fdfa841addfad572176a2ebf75ee5b79fb2b78ef
Luyandra_Branco <94245234+LuyandraBranco@users.noreply.github.com>
2023-05-21 14:53:56 +0100

    App: criação das rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './globalStyles.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './pages/Login';
+import SignIn from './pages/SignIn';
+import Club from './pages/Club';
+import Country from './pages/Country';
+import Details from './pages/Details';
+import League from './pages/Details';
+import Season from './pages/Details';
 
 function App() {
 
   return (
-    <></>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/SignIn" component={SignIn} />
+        <Route path="/Club" component={Club} />
+        <Route path="/Country" component={Country} />
+        <Route path="/Details" component={Details} />
+        <Route path="/League" component={League} />
+        <Route path="/Season" component={Season} />
+      </Switch>
+    </Router>
   )
 }
 
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 export default function Login(){
 
     return(
-        <></>
+        <>
+        <h1>Login</h1>
+        </>
     )
 }
